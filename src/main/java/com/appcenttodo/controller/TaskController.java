d3af50ba9232805feba26b7dbf78be1efaedde5d
Mehmet Alper Karabay <alperkarabay0671@gmail.com>
2022-06-09 17:48:18 +0300

    cross origins added for react connection

--- src/main/java/com/appcenttodo/controller/TaskController.java
+++ src/main/java/com/appcenttodo/controller/TaskController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "${todo.base-url}")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskController {
     @Autowired
     private TaskServiceImpl taskService;
--- src/main/java/com/appcenttodo/controller/UserController.java
+++ src/main/java/com/appcenttodo/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "${user.base-url}")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     @Autowired
     private UserServiceImpl userService;
