6e5a70a0346e3dfb0106b0e785e9a5abf93b99f8
Mehmet Alper Karabay <alperkarabay0671@gmail.com>
2022-06-10 15:09:25 +0300

    unit tests

--- src/test/java/com/appcenttodo/AppcentToDoApplicationTests.java
+++ src/test/java/com/appcenttodo/AppcentToDoApplicationTests.java
@@ -1,11 +1,16 @@
 package com.appcenttodo;
 
 import com.appcenttodo.enums.TaskStatus;
+import com.appcenttodo.models.Log;
 import com.appcenttodo.models.Task;
+import com.appcenttodo.models.UserDetail;
+import com.appcenttodo.repositories.LogRepository;
 import com.appcenttodo.repositories.TaskRepository;
+import com.appcenttodo.repositories.UserRepository;
 import com.appcenttodo.services.LogServiceImpl;
 import com.appcenttodo.services.TaskService;
 import com.appcenttodo.services.TaskServiceImpl;
+import com.appcenttodo.services.UserServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,22 +25,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Collections;
 import java.util.List;
 
+import static org.mockito.Mockito.*;
 
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class TodoAppApplicationTests {
 
 	@InjectMocks
 	private TaskServiceImpl taskService;
-
 	@InjectMocks
 	private LogServiceImpl logService;
+	@InjectMocks
+	private UserServiceImpl userService;
 	@Mock
 	private Task task;
 	@Mock
+	private Log log;
+	@Mock
+	private UserDetail user;
+
+
+	@Mock
+	private LogRepository logRepo;
+	@Mock
 	private TaskRepository taskRepo;
+	@Mock
+	private UserRepository userRepo;
 
 	@Test
 	public void testAddTask() throws Exception {
@@ -55,20 +70,40 @@ class TodoAppApplicationTests {
 	}
 
 	@Test
-	public void testGetAllTasksWithNoData() {
-		Task taskTest = new Task();
-		taskTest.setTaskTitle("New Task");
-		taskTest.setStatus(TaskStatus.TO_DO);
-		taskTest.setId(1);
-		when(taskRepo.findAll()).thenReturn(Collections.emptyList(),Collections.singletonList(taskTest));
-		taskService.addTask(taskTest);
-		List<Task> todoList =taskService.getTasks();
-		Assertions.assertEquals(1,todoList.size());
-		Assertions.assertEquals(todoList.get(0),Task.builder().id(1).taskTitle("test").build());
+	public void testDeleteTask() throws Exception {
+		/*Task todoTest = new Task();
+		todoTest.setTaskTitle("test-name");
+		todoTest.setStatus(TaskStatus.TO_DO);
+		todoTest.setId(1L);
+		taskService.addTask(todoTest);
+
+		taskService.deleteTask(1L);
+		Task taskToDelete = taskRepo.findById(1L).orElseThrow(() -> new Exception("Task not found with id : "+ todoTest.getId()));
+
+		Assertions.assertEquals(null,taskToDelete);*/
+	}
+
+	@Test
+	public void testGetAllLogs() {
+		Log logTest = new Log();
+		logTest.setDescription("test-desc");
+
+		when(logRepo.findAll()).thenReturn(Collections.singletonList(logTest));
+		List<Log> logList =logService.getLogs();
+		Assertions.assertEquals(1,logList.size());
+
 	}
 
 	@Test
 	public void testUpdateTask() {
 	}
+	@Test
+	public void register() {
+		UserDetail registerTest = new UserDetail();
+		registerTest.setUsername("test-name");
+		registerTest.setPassword("password");
+		userService.register(registerTest);
+		Assertions.assertEquals(false,userRepo.findById(1L).isPresent());
 
+	}
 }
